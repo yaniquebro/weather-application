aab041a893855a42f5226e1e3c4b676553dfbad0
Yanique Brooks <yaniquebro@gmail.com>
2024-07-08 15:47:06 -0400

    fetch request

--- assests/script.js
+++ assests/script.js
@@ -1,15 +1,32 @@
 //api key
-const apiKey = 9ad181ccac33af9759bb52f96e34e1cf;
+const apiKey = "9ad181ccac33af9759bb52f96e34e1cf";
 
 //variables
 const userCity = document.querySelector("#city-search");
 const searchButton = document.querySelector(".searchBtn");
-const cityHistory = document.querySelector(".searchHistory");
+const searchHistory = document.querySelector(".searchHistory");
 const weatherForecast = document.querySelector(".weatherToday");
 const renderCards = document.querySelector(".renderCards");
 
-let city;
+searchHistory();
 
-const queryURL = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+//fetch request
+function getCity(city) {
+    const queryURL = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    fetch(queryURL)
 
-fetch(queryURL)
+    .then((response) => response.json())
+    .then ((data) => {
+        displayWeather(data);
+        saveSearches(city);
+        getForecast(city);
+    })
+
+    .catch((error) => {
+        console.log("Error fetching data: ", error);
+        alert("Location not found" + city);
+
+    });
+}
+
+
