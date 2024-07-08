d43107de86c3c2a4ca929fb14bc6fd822743ec68
Yanique Brooks <yaniquebro@gmail.com>
2024-07-08 17:36:28 -0400

    debug at 116

--- assests/script.js
+++ assests/script.js
@@ -2,22 +2,22 @@
 const apiKey = "9ad181ccac33af9759bb52f96e34e1cf";
 
 //variables
-const userCity = document.querySelector("#city-search");
+const userCity = document.querySelector("#citySearch");
 const searchButton = document.querySelector(".searchBtn");
 const searchHistory = document.querySelector(".searchHistory");
 const weatherForecast = document.querySelector(".weatherToday");
 const renderCards = document.querySelector(".renderCards");
 
-searchHistory();
+rendersearchHistory();
 
 //fetch request
 function getCity(city) {
-    const queryURL = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
     fetch(queryURL)
 
     .then((response) => response.json())
     .then ((data) => {
-        displayWeather(data);
+        renderWeather(data);
         saveSearches(city);
         getForecast(city);
     })
@@ -46,23 +46,56 @@ const getForecast = (city) => {
 
 const renderWeather = (data) => {
     let liveDate = new Date().toLocaleDateString();
-    const temp = (data.main.temp - 306.3) * 1.8 + 32;
+    const temp = (data.main.temp - 273.15) * 1.8 + 32;
 
     weatherForecast.innerHTML = `
         <h1 class="fw-bold m-2">${data.name} (${liveDate})</h1>
         <p class="m-2">Temperature: ${temp.toFixed(2)} °F
         </br>Wind Speed: ${data.wind.speed} MPH 
         </br>Humidity: ${data.main.humidity}%</p>`;
- 
     }
 
+//render five day forecast
+const renderForecast = function (data) {
+    renderCards.innerHTML = "";
 
+    for (let i = 0; i < 5; i++) {
+        const forecast = data.list[i * 8];
+        const date = new Date();
+        date.setDate(date.getDate() + (i + 1));
+        const tempF = (forecast.main.temp - 273.15) * 1.8 + 32;
 
+        const card = document.createElement("div");
+        card.classList.add("card", "m-3");
+        card.innerHTML = `
+        <h4>${date.toLocaleDateString("en-US", {
+            month: "numeric",
+            day: "numeric",
+            year: "numeric",
+        })}</h4> 
+            <div>${createIcon.outerHTML}</div>
+            <p>Temp: </br>${tempF.toFixed(2)}°F</p>
+            <p>Wind: </br>${forecast.wind.speed} MPH</p>
+            <p>Humidity: </br>${forecast.main.humidity}%`;
+
+        renderCards.appendChild(card);
+    }
+};
+
+// local storage
+function saveSearches(city) {
+    const searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+    if (!searchHistory.includes(city)) {
+        searchHistory.unshift(city);
+        localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+        renderCityBtn(city);
+    }
+}
 
 //search history
 function renderCityBtn(city) {
     const currentBtn = Array.from(
-        searchHistory.querySelectorAll("button")).find((btn) => btn.textContent === city);
+        searchHistory.querySelectorAll("btn")).find((btn) => btn.textContent === city);
 
         if (!currentBtn) {
             const btn = document.createElement("button");
@@ -71,9 +104,31 @@ function renderCityBtn(city) {
             button.setAttribute("data-city", city);
             button.addEventListener("click", () => {
                 getCity(city);
-                retrieveForecast(city);
+                getForecast(city);
             });
-            cityHistory.prepend(button);
+            searchHistory.prepend(button);
         }
     }
+
+    //search btn
+searchButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    const city = userCity.value.trim();
+    if (city) {
+        renderCityBtn(city);
+        getCity(city);
+        getForecast(city);
+        userCity.value = "";
+    } else { 
+        alert("enter city to search");
+    }
+});
+
+function rendersearchHistory() {
+    const searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+    searchHistory.forEach((city) => {
+        renderCityBtn(city);
+    });
+}
+
 
